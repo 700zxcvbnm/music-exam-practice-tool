7379e0fe23f02ad0eea48714ec27565d445e14ed
700zxcvbnm <xtrimiguntinggamin.g@gmail.com>
2024-05-31 19:53:36 +0800

    prototype

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
-const quizData = [
-    {
-        audio: "audio1",
-        label: ["person", "song name"],
-        answers: ["lololololol", "lolol"],
-    }, {
-        audio: "audio2",
-        label: ["idfk", "mckzv;nb"],
-        answers: ["??????", "Whar"],
-    },
-];
-
 const content = document.getElementById("content");
 var questionDisplay;
 var inputs;
 
-var currentQuestion;
+var currentQuestion = 0;
 var questionOrder;
-var userAnswers;
+var userAnswers = new Array(quizData.length);
 
 // util
 function generateOrder() {
@@ -25,6 +13,16 @@ function generateOrder() {
     order.sort(() => Math.random() - 0.5);
 
     return order;
+}
+
+function filterText(text) {
+    return text.replace(/[\s\.-]+/g,'').toLowerCase();
+}
+
+function percentToColor(percent) {
+    var hue = (percent * 120).toString(10);
+    console.log(hue);
+    return `hsl(${hue}, 100%, 34%)`;
 }
 
 // main
@@ -38,37 +36,50 @@ function loadQuestion() {
     </audio>
     `;
 
+    var inputTable = document.createElement("table");
+
     inputs.innerHTML = ``;
     for (var i = 0; i < question.answers.length; i++) {
-        inputs.innerHTML += `<br> ${question.label[i]} `;
+        var inputRow = inputTable.insertRow();
 
+        var labelCell = inputRow.insertCell();
+        labelCell.className += "left";
+        labelCell.appendChild(document.createTextNode(question.label[i]));
+
+        var inputCell = inputRow.insertCell();
         var input = document.createElement("input");
+        input.classList.add("center", "mono");
         input.id = "input" + i;
 
-        inputs.appendChild(input);
+        inputCell.appendChild(input);
     }
+
+    inputs.appendChild(inputTable);
 }
 
 function submitAnswer() {
     const question = quizData[questionOrder[currentQuestion]];
     userAnswers[currentQuestion] = new Array(question.answers.length).fill("");
 
-    for (var input of inputs.children) {
+    inputs.querySelectorAll("tr").forEach((row) => {
+        var input = row.cells[1].querySelector("input");
         var inputNumber = input.id.replace(/^\D+/g, '');
 
-        userAnswers[currentQuestion][inputNumber] = input.value;
-    }
+        userAnswers[currentQuestion][inputNumber] = filterText(input.value);
+    });
 }
 
 function showResults() {
-    content.innerHTML = `<h2>Results (you uhh finished)</h2>`; //table
+    content.innerHTML = `<h2>Results</h2>`;
 
+    var userScore = 0;
+    var totalScore = 0;
     for (var i = 0; i < quizData.length; i++) {
         const div = document.createElement("div");
         const question = quizData[questionOrder[i]];
 
         div.innerHTML += (i > 0 ? "<br>" : "");
-        div.innerHTML += `<h3>question ${i + 1}</h3>`;
+        div.innerHTML += `<h3 class="mono">question ${i + 1}</h3>`;
         div.innerHTML += `
         <audio controls>
             <source src=${question.audio} type="audio/mp3">
@@ -80,34 +91,73 @@ function showResults() {
         //answer row
         var answerRow = tbl.insertRow();
         var asdf1 = answerRow.insertCell();
-        asdf1.appendChild(document.createTextNode("The answer was..."));
+        asdf1.appendChild(document.createTextNode("答案是..."));
+        asdf1.className += "left";
 
         question.answers.forEach((answer) => {
             var td = answerRow.insertCell();
-            td.appendChild(document.createTextNode(answer));
+
+            if (Array.isArray(answer)) {
+                td.appendChild(document.createTextNode(answer[0]));
+            } else {
+                td.appendChild(document.createTextNode(answer));
+            }
+
+            td.classList.add("left", "mono");
         });
 
         //user answer row
         var userRow = tbl.insertRow();
         var asdf2 = userRow.insertCell();
-        asdf2.appendChild(document.createTextNode("and you submitted"));
+        asdf2.appendChild(document.createTextNode("而你答了"));
+        asdf2.className += "left";
 
-        userAnswers[i].forEach((answer) => {
+        var userQuestionScore = 0;
+        var questionScore = 0;
+        userAnswers[i].forEach((answer, inputIndex) => {
             var td = userRow.insertCell();
-            td.appendChild(document.createTextNode(answer));
+            td.appendChild(document.createTextNode(answer === "" ? "(none)" : answer));
+            td.classList.add("left", "mono");
+
+            var isCorrect = false;
+            if (Array.isArray(question.answers[inputIndex])) {
+                question.answers[inputIndex].forEach((correctAnswer) => {
+                    if (answer === filterText(correctAnswer)) {
+                        isCorrect = true;
+                    }
+                })
+            } else {
+                isCorrect = answer === filterText(question.answers[inputIndex]);
+            }
+
+            if (isCorrect) {
+                userQuestionScore += question.scores[inputIndex];
+                td.style.color = "green";
+            } else {
+                td.style.color = "red";
+            }
+
+            questionScore += question.scores[inputIndex];
         });
 
+        var scoreColor = percentToColor(userQuestionScore / questionScore);
+        div.querySelector("h3").innerHTML += ` <p style="color:${scoreColor}">(${userQuestionScore}/${questionScore})</p>`
+
+        userScore += userQuestionScore;
+        totalScore += questionScore;
         div.appendChild(tbl);
         content.appendChild(div);
     }
 
-    content.innerHTML += `<br><button onclick="initialize()">try again</button>`;
+    content.innerHTML += `<br><h3 style="color:${scoreColor}">總分: ${userScore}/${totalScore}</h3>`;
+    content.innerHTML += `<button onclick="location.reload()">try again <i class="fa-solid fa-arrow-right"></i></button>`;
 }
 
 function startQuiz() {
     content.innerHTML = `<div id="question">what happened on 1984</div>
 
     <div id="inputs"></div>
+    <br>
     <button id="submit">submit</button>`;
 
     questionDisplay = content.querySelector("#question");
@@ -129,18 +179,4 @@ function startQuiz() {
     loadQuestion();
 }
 
-function initialize() {
-    currentQuestion = 0;
-    userAnswers = new Array(quizData.length);
-
-    content.innerHTML = `
-        gaspgsa
-        <br>
-        <br>
-        <button id="start">start quiz</button>
-    `;
-
-    content.querySelector("#start").addEventListener("click", startQuiz);
-}
-
-initialize();
+content.querySelector("#start").addEventListener("click", startQuiz);
---
+++ songs.js
@@ -0,0 +1,128 @@
+const quizData = [
+    {
+        audio: "audio/song1.mp3",
+        label: ["曲名", "時期"],
+        answers: [["格雷果聖歌", "葛雷果聖歌"], "中世紀"],
+        scores: [2, 2],
+    }, {
+        audio: "audio/song2.mp3",
+        label: ["曲名", "作曲家", "時期"],
+        answers: ["四季小提琴協奏曲", ["韋瓦第", "Vivaldi"], "巴洛克"],
+        scores: [2, 1, 1],
+    }, {
+        audio: "audio/song3.mp3",
+        label: ["曲名", "作曲家", "時期"],
+        answers: ["D小調觸技曲", ["巴哈", "巴赫", "Bach"], "巴洛克"],
+        scores: [2, 1, 1],
+    }, {
+        audio: "audio/song4.mp3",
+        label: ["曲名", "作曲家", "時期"],
+        answers: ["G大調小步舞曲", ["巴哈", "巴赫", "Bach"], "巴洛克"],
+        scores: [2, 1, 1],
+    }, {
+        audio: "audio/song5.mp3",
+        label: ["曲名", "作曲家", "時期"],
+        answers: ["神劇彌賽亞", ["韓德爾", "Handel"], "巴洛克"],
+        scores: [2, 1, 1],
+    }, {
+        audio: "audio/song6.mp3",
+        label: ["曲名", "作曲家", "時期"],
+        answers: ["水上音樂", ["韓德爾", "Handel"], "巴洛克"],
+        scores: [2, 1, 1],
+    }, {
+        audio: "audio/song7.mp3",
+        label: ["曲名", "作曲家", "時期"],
+        answers: [["No.94 驚愕交響曲", "第94號驚愕交響曲", "第九十四號驚愕交響曲"], ["海頓", "Haydn"], "古典"],
+        scores: [2, 1, 1],
+    }, {
+        audio: "audio/song8.mp3",
+        label: ["曲名", "作曲家", "時期"],
+        answers: ["土耳其進行曲", ["莫札特", "Mozart"], "古典"],
+        scores: [2, 1, 1],
+    }, {
+        audio: "audio/song9.mp3",
+        label: ["曲名", "作曲家", "時期"],
+        answers: ["魔笛夜后", ["莫札特", "Mozart"], "古典"],
+        scores: [2, 1, 1],
+    }, {
+        audio: "audio/song10.mp3",
+        label: ["曲名", "作曲家", "時期"],
+        answers: ["小夜曲 K.525", ["莫札特", "Mozart"], "古典"],
+        scores: [2, 1, 1],
+    }, {
+        audio: "audio/song11.mp3",
+        label: ["曲名", "作曲家", "時期"],
+        answers: [["給愛麗絲", "給愛麗斯"], ["貝多芬", "Beethoven"], "古典"],
+        scores: [2, 1, 1],
+    }, {
+        audio: "audio/song12.mp3",
+        label: ["曲名", "作曲家", "時期"],
+        answers: [["No.5 命運交響曲", "第5號命運交響曲", "第五號命運交響曲"], ["貝多芬", "Beethoven"], "古典"],
+        scores: [2, 1, 1],
+    }, {
+        audio: "audio/song13.mp3",
+        label: ["曲名", "作曲家", "時期"],
+        answers: [["No.9 合唱交響曲", "第9號合唱交響曲", "第九號合唱交響曲"], ["貝多芬", "Beethoven"], "古典"],
+        scores: [2, 1, 1],
+    }, {
+        audio: "audio/song14.mp3",
+        label: ["曲名", "演奏形式 (4字)", "族別"],
+        answers: ["祈禱小米豐收歌", "八部合音", ["布農族", "布農"]],
+        scores: [2, 1, 1],
+    }, {
+        audio: "audio/song15.mp3",
+        label: ["曲名", "演唱者", "時期"],
+        answers: ["老人飲酒歌", "郭英男", ["阿美族", "阿美"]],
+        scores: [2, 1, 1],
+    }, {
+        audio: "audio/song16.mp3",
+        label: ["曲名", "作曲家 (3字)", "作曲家 (2字)"],
+        answers: ["梁祝小提琴協奏曲", "何占豪", "陳鋼"],
+        scores: [1, 1, 1],
+    }, {
+        audio: "audio/song17.mp3",
+        label: ["曲名", "作曲家"],
+        answers: ["梆笛協奏曲", "馬水龍"],
+        scores: [2, 1],
+    }, {
+        audio: "audio/song18.mp3",
+        label: ["曲名", "樂器"],
+        answers: ["十面埋伏", "琵琶"],
+        scores: [2, 1],
+    }, {
+        audio: "audio/song19.mp3",
+        label: ["曲名", "樂器"],
+        answers: ["鼓吹樂", "北管"],
+        scores: [2, 1],
+    }, {
+        audio: "audio/song20.mp3",
+        label: ["曲名", "起源和歌謠類別 (共4字)"],
+        answers: ["丟丟銅仔", "宜蘭民謠"],
+        scores: [2, 1],
+    }, {
+        audio: "audio/song21.mp3",
+        label: ["曲名", "時期 (2字)"],
+        answers: ["雨夜花", "日治"],
+        scores: [2, 1],
+    }, {
+        audio: "audio/song22.mp3",
+        label: ["曲名", "時期 (2字)"],
+        answers: ["望春風", "日治"],
+        scores: [2, 1],
+    }, {
+        audio: "audio/song23.mp3",
+        label: ["曲名", "時期"],
+        answers: ["燒肉粽", "光復"],
+        scores: [2, 1],
+    }, {
+        audio: "audio/song24.mp3",
+        label: ["曲名", "起源"],
+        answers: ["甘美朗", "印尼"],
+        scores: [2, 1],
+    }, {
+        audio: "audio/song25.mp3",
+        label: ["曲名", "起源"],
+        answers: ["西塔琴", "印度"],
+        scores: [2, 1],
+    },
+];
