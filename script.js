c9ae03c14b3cb81ae46465599d054082b9d975f0
700zxcvbnm <xtrimiguntinggamin.g@gmail.com>
2024-06-05 00:29:51 +0800

    add background toggle
    Tally Hall - Turn The Lights Off

--- script.js
+++ script.js
@@ -301,3 +301,22 @@ if (getCookie("inputData") != null && getCookie("orderData") != null) { //found 
 } else {
     content.querySelector("#start").addEventListener("click", startQuiz); //no data / data broken D:
 }
+
+var backgroundToggled = true;
+document.querySelector("#bgtoggle").addEventListener("click", function() {
+    if (backgroundToggled) {
+        document.querySelector("#bg").remove();
+        document.querySelector("#bg2").remove();
+    } else {
+        var bg1 = document.createElement("div");
+        var bg2 = document.createElement("div");
+
+        bg1.id = "bg";
+        bg2.id = "bg2";
+
+        document.body.appendChild(bg1);
+        document.body.appendChild(bg2);
+    }
+
+    backgroundToggled = !backgroundToggled;
+});
